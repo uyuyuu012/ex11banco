e0abc6217f6e90799c4f07e2e066a077b6e281ca
guitama.games@gmail.com <guitama.games@gmail.com>
2025-11-27 12:25:51 -0300

    corrigindo oq deu e o enildo é um desgraçado

--- Ex11.sql
+++ Ex11.sql
@@ -147,6 +147,7 @@ call insert_cidade1('Osasco');
 call insert_cidade1('Pirituba');
 call insert_cidade1('Lapa');
 call insert_cidade1('Ponta Grossa');
+
 -- atividade 3 
 
 delimiter $$
@@ -163,14 +164,14 @@ call insert_estado1('SP');
 call insert_estado1('RJ');
 call insert_estado1('RS');
 
+select * from tbEstado;
 
 -- atividade 4
 
 delimiter $$
-create procedure insert_bairro1 (in InBairro varchar(200))
+create procedure insert_bairro1 (in InBairro varchar(200) )
 begin
-	-- Inserindo valores na tbbairro
-	insert into tbBairro(Bairro)
+		insert into tbBairro(Bairro)
 		values
         (InBairro);
 end;
@@ -180,19 +181,21 @@ call insert_bairro1('Aclimação');
 call insert_bairro1('Capão Redondo');
 call insert_bairro1('Pirituba');
 call insert_bairro1('Liberdade');
-    
+
+select * from tbBairro;
+
 -- atividade 5
 
 delimiter $$
 create procedure insert_produto (in InCodigo bigint, in InNome varchar(200), in InValor decimal(7,2), in InQuantidade int)
 begin
 	-- Inserindo valores na tbproduto
-	insert into tbProduto
+		insert into tbProduto
 		values
 		(InCodigo, InNome,InValor,InQuantidade);
-
 end;
 $$ delimiter ;
+
 call insert_produto (12345678910111, 'Rei do Papel Mache', 54.61, 120);
 call insert_produto (12345678910112, 'Bolinha de Sabão', 100.45, 120);
 call insert_produto (12345678910113, 'Carro Bate', 44.00, 120);
@@ -202,14 +205,18 @@ call insert_produto (12345678910116, 'Boneco do Hitler', 124.00, 200);
 call insert_produto (12345678910117, 'Farinha de Suruí', 50.00, 200);
 call insert_produto (12345678910118, 'Zelador do Cemitério', 24.50, 120);
 
+select * from tbProduto
+
+
 -- atividade 6
 
 
-delimiter $$
+
+    delimiter $$
 create procedure insert_endereco (in InLogradouro varchar(200), in InBairro varchar(200), in InCidade varchar(200), in InUf varchar(2), in InCep int)
 begin
 	-- Inserindo valores na tbendereco
-
+	
 	insert into tbBairro(Bairro)
 		values
         (InBairro);
@@ -236,17 +243,18 @@ call insert_endereco('Rua Ximbú','Penha', 'Rio de Janeiro', 'RJ', 12345054);
 call insert_endereco('Rua Piu XI','Penha', 'Campinas', 'SP', 12345055);
 call insert_endereco('Rua Chocolate','Aclimação', 'Barra Mansa', 'RJ', 12345056);
 call insert_endereco('Rua Pão na Chapa','Barra Funda', 'Ponta Grossa', 'RS', 12345057);
-    
+
 -- atividade 7
  
 
 delimiter $$
 create procedure insert_cliente (in InNomeCli varchar(200), in InNumEnd int, in InCompEnd varchar(200), in InCepCli int, in InId int, in InCPF decimal(11,0), in InRG decimal(9,0), in InRG_DIG char(1), in InNasc datetime)
 begin
-	-- Inserindo valores na tbcliente
-	insert into tbCliente (NomeCli, NumEnd, CompEnd, CepCli)
+
+		insert into tbCliente (NomeCli, NumEnd, CompEnd, CepCli)
 		values 
         (InNomeCli, InNumEnd, InCompEnd, InCepCli);
+	
 
 	insert into tbCliente_PF(Id, CPF, RG, RG_DIG, Nasc)
 		values	 
@@ -260,63 +268,47 @@ call insert_cliente('Marciano', 744, null, 12345054, 3, 12345678913, 12345680, 0
 call insert_cliente('Lança Perfume', 128, null, 12345059, 4, 12345678914, 12345681, 'X', '2004-04-05');
 call insert_cliente('Remédio Amargo', 2585, null, 12345058, 5, 12345678915, 12345682, 0, '2002-07-15');
 
+select * from tbCliente;
+
 -- atividade 8
 
-delimiter $$
-create procedure insert_clientepj ()
-begin
-	-- Inserindo valores na tbcliente_pf
-	insert into tbCliente_PJ
-		values	 
-        ( 6, 12345678912345, 98765432198),
-        ( 7, 12345678912346, 98765432199),
-        ( 8, 12345678912347, 98765432100),
-        ( 9, 12345678912348, 98765432101),
-        ( 10, 12345678912349, 98765432102);
-	insert into tbEndereco(Logradouro, Bairro, Cidade, UF)
-		values
-        ('Av', 'Lapa', 'Campinas', 'SP'),
-        ('Av', 'Penha', 'Rio de Janeiro', 'RJ'),
-        ('Rua dos Amores', 'Sei Lá', 'Recife', 'PE'),
-        ('Rua dos Amores', 'Sei Lá', 'Recife', 'PE'),
-        ('Rua dos Amores', 'Sei Lá', 'Recife', 'PE');
-end;
-$$ delimiter ;
-call insert_clientepj();
+DELIMITER $$
+create procedure insert_clientepj (IN inCNPJ BIGINT, IN inIE BIGINT, IN inCep INT, IN inLogradouro VARCHAR(200), IN inNumEnd INT, IN inCompEnd VARCHAR(50), IN inBairro VARCHAR(200), IN inCidade VARCHAR(200), IN inUF VARCHAR(2))
+BEGIN
+    INSERT INTO tbCliente_PJ (CNPJ, IE)
+    VALUES (inCNPJ, inIE);
+
+    INSERT INTO tbEndereco (Logradouro, Bairro, Cidade, UF)
+    VALUES (inLogradouro, inBairro, inCidade, inUF);
+END
+$$ DELIMITER ;
+
+CALL insert_clientepj(12345678912345,98765432198,0,'Av Brasil',0,NULL,'Lapa','Campinas','SP');
+CALL insert_clientepj(12345678912346,98765432199,0,'Av Paulista' ,0,NULL,'Penha','Rio de Janeiro','RJ');
+CALL insert_clientepj(12345678912347,98765432100,0,'Rua dos Amores',0,NULL,'Sei Lá','Recife','PE');
+CALL insert_clientepj(12345678912348,98765432101,0,'Rua dos Amores',0,NULL,'Sei Lá','Recife','PE');
+CALL insert_clientepj(12345678912349,98765432102,0,'Rua dos Amores',0,NULL,'Sei Lá','Recife','PE');
+
+select * from tbCliente_PJ;
 
 -- atividade 9
 
-delimiter $$
-create procedure insert_compra()
-begin 
-	-- Inserindo valores na tabela compra
-	insert into tbCompra(NotaFiscal, Fornecedor, DataCompra, CodigoBarras, ValorItem, Qtd,QtdTotal,ValorTotal)
-		values
-        (8459, 'Amoroso e Doce', STR_TO_DATE('2018/05/01', '%Y/%m/%d'), 12345678910111, 22.22, 200, 700, 21944.00),
-        (2482, 'Revenda Chico Loco', STR_TO_DATE('2020/04/22', '%Y/%m/%d'), 12345678910112, 40.50, 180, 180, 7290.00),
-        (21563, 'Marcelo Dedal', STR_TO_DATE('2020/07/12', '%Y/%m/%d'), 12345678910113, 3.00, 300, 300, 900.00),
-        (8459, 'Amoroso e Doce', STR_TO_DATE('2018/05/01', '%Y/%m/%d'), 12345678910114, 35.00, 500, 700, 21944.00),
-        (156354, 'Revenda Chico Loco', STR_TO_DATE('2021/11/23', '%Y/%m/%d'), 12345678910115, 54.00, 350, 350, 18900.00);
-      
-end;
-$$ delimiter ;
-call insert_compra();
+DELIMITER $$
+CREATE PROCEDURE insert_compra(IN inNotaFiscal INT,IN inFornecedor VARCHAR(200),IN inDataCompra DATE,IN inCodigoBarras BIGINT,IN inValorItem DECIMAL(10,2),IN inQtd INT,IN inQtdTotal INT,IN inValorTotal DECIMAL(12,2))
+BEGIN
+    INSERT INTO tbCompra(NotaFiscal, Fornecedor, DataCompra, CodigoBarras, ValorItem, Qtd, QtdTotal, ValorTotal)
+    VALUES (inNotaFiscal, inFornecedor, inDataCompra, inCodigoBarras, inValorItem, inQtd, inQtdTotal, inValorTotal);
+END$$
+DELIMITER ;
+CALL insert_compra(8459, 'Amoroso e Doce', STR_TO_DATE('2018/05/01', '%Y/%m/%d'), 12345678910111, 22.22, 200, 700, 21944.00);
+CALL insert_compra(2482, 'Revenda Chico Loco', STR_TO_DATE('2020/04/22', '%Y/%m/%d'), 12345678910112, 40.50, 180, 180, 7290.00);
+CALL insert_compra(21563, 'Marcelo Dedal', STR_TO_DATE('2020/07/12', '%Y/%m/%d'), 12345678910113, 3.00, 300, 300, 900.00);
+CALL insert_compra(8459, 'Amoroso e Doce', STR_TO_DATE('2018/05/01', '%Y/%m/%d'), 12345678910114, 35.00, 500, 700, 21944.00);
+CALL insert_compra(156354, 'Revenda Chico Loco', STR_TO_DATE('2021/11/23', '%Y/%m/%d'), 12345678910115, 54.00, 350, 350, 18900.00);
 
-delimiter //
-create procedure insert_itemcompra()
-begin 
-	-- Inserindo valores na tabela item compra
-	insert into tbitem_compra
-		values
-        (8459, 12345678910111, 200, 22.22),
-        (2482, 12345678910112, 180, 40.50),
-        (21563, 12345678910113, 300, 3.00),
-        (8459, 12345678910114, 500, 35.00),
-        (156354, 12345678910115, 350, 54.00);
-    
-end;
-// delimiter ;
-call insert_itemcompra;
+select * from tbCompra;
+
+
 
 -- atividade 10
 
@@ -397,21 +389,14 @@ call insert_nf(360, 4);
 -- atividade 12
 
 -- inserindo novos produtos na tbproduto
-delimiter //
-create procedure insert_produto2(
-	in p_codigobarras decimal(14,0),
-    in p_nome varchar(200),
-    in p_valorunit decimal(7,2),
-    in p_qtd int
-)
+delimiter $$
+create procedure insert_Produto2(	in p_codigobarras decimal(14,0),    in p_nome varchar(200),    in p_valorunit decimal(7,2),    in p_qtd int)
 begin
-
-	insert into tbproduto
+	insert into tbProduto
 		values
         (p_codigobarras, p_nome, p_valorunit, p_qtd);
-
 end;
-// delimiter ;
+$$ delimiter ;
 call insert_produto2(12345678910130, 'Camiseta de Poliéster', 35.61, 100);
 call insert_produto2(12345678910131, 'Blusa Frio Moletom', 200.00, 100);
 call insert_produto2(12345678910132, 'Vestido Decote Redondo', 144.00, 50);
@@ -465,12 +450,6 @@ end;
 // delimiter ;
 call select_produto();
 
--- atividade 16
+    
 
-create table tbproduto_historico like tbproduto;
-
--- atividade 17
-
-alter table tbproduto_historico 
-	add column Ocorrencia varchar(20) null, 
-    add column Atualizacao datetime null;
+    
